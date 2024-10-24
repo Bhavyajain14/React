6a59366358b883ebbc0d2a1c50bf513d2eb15113
Bhavyajain14 <bhavyajain999478@gmail.com>
2024-10-24 23:17:07 +0530

    tailwind with react

--- 03tailwind_Props/src/App.jsx
+++ 03tailwind_Props/src/App.jsx
@@ -2,15 +2,17 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/card";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-rose-700 bg-green-400 p-4 rounded-xl">
-        Tailwind <T></T>est
+      <h1 className="text-rose-700 bg-green-400 p-4 rounded-xl mb-10">
+        Tailwind test
       </h1>
+      <Card channelname="ChaiAurcode" arr = { } />
     </>
   );
 }
